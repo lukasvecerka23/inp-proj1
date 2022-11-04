45b8cbb5a0d25df98144be16549031a763bfb06e
Lukas Vecerka <vecerkalukas18@gmail.com>
2022-11-04 22:46:52 +0100

    create process for counters, mxs and fsm

--- zadani/src/cpu.vhd
+++ zadani/src/cpu.vhd
@@ -41,7 +41,186 @@ end cpu;
 --                      Architecture declaration
 -- ----------------------------------------------------------------------------
 architecture behavioral of cpu is
+
+  	-- Programovy citac - ukazatel do pameti programu
+  	signal PC_addr : std_logic_vector(12 downto 0) <= (others =>'0');
+  	signal PC_inc : std_logic;
+  	signal PC_dec : std_logic;
+
+  	-- Pointer do pameti dat
+  	signal PTR_addr : std_logic_vector(12 downto 0) <= (12 => '1', others =>'0');
+  	signal PTR_inc : std_logic;
+	signal PTR_inc : std_logic;
+
+  	-- Citac pro zacatek/konec while cyklu
+
+	-- MX1 - vybrani zda se jedno o adresu programu nebo dat
+	signal MX1_sel : std_logic;
+	signal MX1_output : (12 downto 0);
+
+	-- MX2 - hodnota zapisovana do pameti
+	signal MX2_sel : std_logic_vector(1 downto 0);
+	signal MX2_output : (7 downto 0);
+
+	-- FSM
+	type FSM is (
+		S_START,
+		S_FETCH,
+		S_DECODE,
+		S_PTR_INC,
+		S_PTR_DEC,
+		S_VAL_INC,
+		S_VAL_DEC,
+		S_WHILE_START,
+		S_WHILE_END,
+		S_DO_WHILE_START,
+		S_DO_WHILE_END,
+		S_WRITE,
+		S_READ,
+		S_NULL
+	);
+	signal curr_state : FSM := S_START;
+	signal next_state : FSM;
 begin
+
+  	-- Programovy Citac
+  	PC_cnt: process (CLK, RESET, PC_inc, PC_dec)
+  	begin
+    	if RESET = '1' then
+			PC_addr <= (others => '0');
+		elsif (CLK'event) and (CLK = '1') then
+			if (PC_inc = '1') then
+				PC_addr <= PC_addr + 1;
+			elsif (PC_dec = '1') then
+				PC_addr <= PC_addr - 1;		
+			end if;
+		end if;
+  	end process;
+
+	-- Citac pro ukazatel do pameti dat
+	PTR_cnt: process (CLK, RESET, PTR_inc, PTR_dec)
+	begin
+		if RESET = '1' then
+			PTR_addr <= (others => '0');
+		elsif (CLK'event) and (CLK='1') then
+			if (PTR_inc = '1') then
+				PTR_addr <= x"1000" + ((PTR_addr + 1) mod x"1000");
+			elsif (PTR_dec = '1') then
+				PTR_addr <= x"1000" + ((PTR_addr - 1) mod x"1000");
+			end if;
+		end if;
+	end process;
+
+	-- MX1
+	MX1: process (CLK, RESET, MX1_sel)
+	begin
+		if RESET = '1' then
+			MX1_output <= (others => '0');
+		elsif (CLK'event) and (CLK='1') then
+			case MX1_sel is
+				when '0' =>
+					MX1_output <= PC_addr;
+				when '1' =>
+					MX1_output <= PTR_addr;
+				when others =>
+					MX1_output <= (others => '0');
+			end case;
+		end if;
+		DATA_ADDR <= MX1_output;
+	end process;
+
+	-- MX2
+	MX2: process(CLK, RESET, MX2_sel)
+	begin
+		if RESET = '1' then
+			MX2_output <= (others => '0');
+		elsif (CLK'event) and (CLK='1') then
+			case MX2_sel is
+				when "00" =>
+					MX2_output <= IN_DATA;
+				when "01" =>
+					MX2_output <= DATA_RDATA + 1;
+				when "10" =>
+					MX2_output <= DATA_RDATA - 1;
+				when others =>
+					MX2_output <= (others => '0'); 
+			end case;
+		end if;
+		DATA_WDATA <= MX2_OUTPUT;
+	end process;
+
+	--FSM
+	state: process (CLK, RESET EN)
+	begin
+		if RESET = '1' then
+			curr_state <= S_START;
+		elsif (CLK'event) and (CLK='1') then
+			if EN = '1' then
+				curr_state <= next_state;
+			end if;
+		end if;
+    end process;
+
+	next_state: process(state)
+	begin
+		PC_inc <= '0';
+		PC_dec <= '0';
+		PTR_inc <= '0';
+		PTR_dec <= '0';
+		DATA_EN <= '0';
+		DATA_RDWR <= '0';
+		IN_REQ <= '0';
+		OUT_WE <= '0';
+		MX1_sel <= '0';
+		MX2_sel <= "00";
+
+		case curr_state is
+			when S_START =>
+				next_state <= S_FETCH;
+			when S_FETCH =>
+				DATA_EN <= '1';
+				next_state <= S_DECODE;
+			when S_DECODE =>
+				case DATA_WDATA is
+					when x"3E" =>
+						next_state <= S_PTR_INC;
+					when x"3C" =>
+						next_state <= S_PTR_DEC;
+					when x"2B" =>
+						next_state <= S_VAL_INC;
+					when x"2D" =>
+						next_state <= S_VAL_DEC;
+					when x"5B" =>
+						next_state <= S_WHILE_START;
+					when x"5D" =>
+						next_state <= S_WHILE_END;
+					when x"28" =>
+						next_state <= S_DO_WHILE_START;
+					when x"29" =>
+						next_state <= S_DO_WHILE_END;
+					when x"2E" =>
+						next_state <= S_WRITE;
+					when x"2C" =>
+						next_state <= S_READ;
+					when x"00" =>
+						next_state <= S_NULL;
+					when others =>
+						next_state <= S_FETCH
+						PC_inc <= '1';
+				end case;
+			when S_PTR_INC =>
+				PTR_inc <= '1';
+				PC_inc <= '1';
+				next_state <= S_FETCH;
+			when S_PTR_DEC =>
+				PTR_dec <= '1';
+				PC_inc <= '1';
+				next_state <= S_FETCH;
+			when others =>
+				next_state <= S_FETCH;
+				PC_inc <= '1';
+		end case;
+	end process;
 
 end behavioral;
 
