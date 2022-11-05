902756a605118b83151ffc13c19bb40ce8d228bf
Lukas Vecerka <vecerkalukas18@gmail.com>
2022-11-05 15:57:09 +0100

    .

--- zadani/src/cpu.vhd
+++ zadani/src/cpu.vhd
@@ -208,7 +208,7 @@ begin
 		end if;
     end process;
 
-	next_state_logic: process(curr_state, instruction, IN_VLD)
+	next_state_logic: process(curr_state, instruction)
 	begin
 		PC_inc <= '0';
 		PC_dec <= '0';
